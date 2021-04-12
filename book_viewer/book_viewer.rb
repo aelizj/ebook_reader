d6a8b84d9faf2e4371ce4ebbb361b7b0f5c20d89
Anne <anne1365@outlook.com>
2021-04-12 15:47:36 -0700

    Lesson 3 parts 7-8

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -4,6 +4,13 @@ require "tilt/erubis"
 
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @chapters = 
+  @contents = File.readlines("data/toc.txt")
   erb :home
+end
+
+get "/chapters/1" do
+  @title = "Chapter 1"
+  @contents = File.readlines("data/toc.txt")
+  @chapter = File.read("data/chp1.txt")
+  erb :chapter
 end
